e46467fb4b0064342bc261a4877621a5a3a9a487
bocah-tua <163550989+bocah-tua@users.noreply.github.com>
2024-05-25 22:17:23 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+const body = document.body;
+const namaSaya = document.createElement("p");
+namaSaya.innerHTML = "<marquee>Jam Pelayanan Hari Senin - Jum.at Pukul 08:30 sampai 16:30 WIB</marquee>";
+body.append(namaSaya);
